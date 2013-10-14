2cc93b6ea73b0ca6568154c1c494ef7e954f0f63
Jonathan Burnett <jburnett96@gmail.com>
2013-10-14 13:42:59 -0400

    Added initial draft of Section model
    Known Issues:
    • Times field is probably not constructed correctly
    • is_overlapping is untested

--- sgator/models.py
+++ sgator/models.py
@@ -1,4 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Section(models.Model):
+    # Necessary for building schedules
+    name = models.CharField(max_length=200)
+    section = models.CharField(max_length=200)
+    DAYS_OF_WEEK = ('M', 'T', 'W', 'R', 'F')
+    PERIODS = range(1,12)
+    for i in range(1,4):
+        PERIODS.append('E' + str(i))
+    times = models.CharField(max_length=2, choices=DAYS_OF_WEEK, PERIODS)
+    # Example of times:
+    # time = [{'day':'M', 'time':5}, {'day':'W', 'time':5}, {'day':'F', 'time':5}]
 
+    # Extra fields for display
+    course_full_name = models.CharField(max_length=200)
+    building = models.CharField(max_length=200)
+    room = models.CharField(max_length=200)
+    credits = models.CharField(max_length=200)
+    instructor = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+
+    def is_overlapping(self, other):
+        for t in self.times:
+            for t2 in other.times:
+                if t['day'] == t2['day'] && t['time'] == t2['time']:
+                    return True
+        return False
