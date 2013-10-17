271fe4c2bd6800194ff3ce54534040245f4584ec
Jordan Farinas <jfarinas@ufl.edu>
2013-10-16 22:05:52 -0400

    Search Works Fully/UserProfiles Implemented
    -Search now works by section too
    -UserProfiles implemented now mapped to User objects
    -In Django Admin UserProfiles can be viewed in the User's page
    -Schedule Generation page added to views handler

--- course/admin.py
+++ course/admin.py
@@ -1,4 +1,19 @@
-from django.contrib import admin
-from course.models import Course
 
+from course.models import Course
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from course.models import UserProfile
+
+admin.site.unregister(User)
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ UserProfileInline, ]
+
+admin.site.register(User, UserProfileAdmin)
 admin.site.register(Course)
+
+
--- sgator/views.py
+++ sgator/views.py
@@ -19,10 +19,19 @@ def insert_space(string, integer):
 def home(request):
     return render(request, "home.html")
 
-def profile(request):
+def generateSchedule(request):
     context = RequestContext(request)
     if request.user.is_authenticated():
-        return render(request, "profile.html")  # to be edited when more stuff is added to profile page
+        return render(request, "schedule.html")
+    else:
+        return render_to_response('nsi.html', context_instance=context)
+    #ALGORITHM to be implemented or referenced here for this page
+    
+def profile(request):
+    context = RequestContext(request)
+    user_profile = request.user.get_profile()#profile object passed to template - can also be manipulated
+    if request.user.is_authenticated():
+        return render_to_response("profile.html",{"uprofile": user_profile,},context_instance=context)  # to be edited when more stuff is added to profile page
     else:
         return render_to_response('nsi.html', context_instance=context)
 
@@ -34,6 +43,7 @@ def search(request):
     iquery = request.GET.get('q')
     query = iquery.strip()
     criteria = request.GET.get('DEPT')
+    #print query + str(len(query))
     if criteria != "":
         resultsF = Course.objects.filter(dept__exact = criteria)
         
@@ -44,6 +54,10 @@ def search(request):
         elif (not query.isalpha()) and (len(query) == 7 or len(query) == 8):
             tquery = insert_space(query,3)
             resultsF = Course.objects.filter(name__icontains = tquery)
+        elif len(query) == 4:
+            resultsF = Course.objects.filter(section__icontains = iquery)
+        elif len(query) == 2:
+            return render_to_response('nrf.html', context_instance=context)  
         elif len(query) > 1:
 
             if ' ' in query:
@@ -53,9 +67,11 @@ def search(request):
                 resultsF = Course.objects.filter(cinst__icontains = tlname).filter(cinst__icontains = tfname)
             else:
                 resultsF = Course.objects.filter(cinst__icontains = query)
+                
             
-        else: return render_to_response('nrf.html', context_instance=context)  #temporary no results found page
-                                             
+        else:
+            return render_to_response('nrf.html', context_instance=context)  #temporary no results found page
+                                          
     size = len(resultsF)    
     if size < 1:
         return render_to_response('nrf.html', context_instance=context)   
@@ -73,8 +89,7 @@ def pasth(request):
         pq = PyQuery(data)
         for c in pq('td'):
             results.append(pq(c).text())
-            #to be parsed and added to Schedule Model/User Profile Model when created
-
+            #to be parsed and added to Schedule Model of User Profile Model when created
         return render_to_response('pasth.html', {"results": results,}, context_instance=context)
 
     else:
