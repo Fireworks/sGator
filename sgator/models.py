90cbcc3463c301a5d2361c8f8908dd6131dedd26
Jonathan Burnett <jburnett96@gmail.com>
2013-10-21 12:42:58 -0400

    Made is_overlapping more pythonic

--- sgator/models.py
+++ sgator/models.py
@@ -8,7 +8,7 @@ class Section(models.Model):
     PERIODS = map(str, range(1,12)) + ['E1', 'E2', 'E3']
     times = models.CharField(max_length=2, choices=DAYS_OF_WEEK, PERIODS)
     # Example of times:
-    # time = [{'day':'M', 'time':5}, {'day':'W', 'time':5}, {'day':'F', 'time':5}]
+    # times = [{'day':'M', 'time':5}, {'day':'W', 'time':5}, {'day':'F', 'time':5}]
 
     # Extra fields for display
     lecture_or_discussion = models.CharField(max_length=200) # To clarify for display purposes
@@ -20,8 +20,4 @@ class Section(models.Model):
     department = models.CharField(max_length=200)
 
     def is_overlapping(self, other):
-        for t in self.times:
-            for t2 in other.times:
-                if t['day'] == t2['day'] && t['time'] == t2['time']:
-                    return True
-        return False
+        return any(t==t2 for t in self.times for t2 in other.times)
