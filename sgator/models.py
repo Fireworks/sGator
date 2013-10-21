3d5c9e7b1466355194c6dfd22806442901028e74
Evan Bass <evanbbass@ufl.edu>
2013-10-21 13:23:54 -0400

    added schedule class

--- sgator/models.py
+++ sgator/models.py
@@ -2,3 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+class Schedule(models.Model):
+    def __init__(self):
+        self.sections = []
+        self.credits = models.CharField(max_length=200, default='0')
+        # total credits in schedule
+
+    def add(self,course):
+        for x in range(len(self.sections)):
+            if course.overlaps(self.sections(x)):
+                return False
+        self.sections[course.section] = course
+        self.credits = self.credits + course.credits
+        return True
+
+    def remove(self,course):
+        del self.sections[course.section]
+        self.credits = self.credits - course.credits
+        # subtract out credits of course
+
+    def returnSchedule(self):
+        return self.sections.values()
+
+    def returnCredits(self):
+        return self.credits
