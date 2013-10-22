b3e0e283d78a7c4cf9898fad238c454a4add29f7
Jonathan Burnett <jburnett96@gmail.com>
2013-10-22 15:25:10 -0400

    Added comment to models, though will change soon

--- sgator/models.py
+++ sgator/models.py
@@ -19,5 +19,6 @@ class Section(models.Model):
     instructor = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
 
+    # example use: class1.overlaps(class2), returns True if they overlap
     def overlaps(self, other):
         return any(t==t2 for t in self.times for t2 in other.times)
