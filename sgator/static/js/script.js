247deeff691fa579593a25d628cd444f82fd1930
Justin <justin@mysticrealms.net>
2013-10-28 00:04:27 -0400

    Ajax post testing from courses page

--- sgator/static/js/script.js
+++ sgator/static/js/script.js
@@ -2,8 +2,22 @@ var courses = [];
 
 $(document).ready(function(){
 	$(document).on('click', '.course-add', function(){
-		$.toast('Course added! View added courses on Create Schedule page.', {type: 'success'});
 		var cid = jQuery(this).attr('course-id');
 		courses.push(cid);
+
+		var json = JSON.stringify(courses);
+		var csrftoken = $.cookie('csrftoken');
+		
+		$.ajax({
+			headers: {"X-CSRFToken": csrftoken},
+			url: '/schedule/',
+			type: 'POST',
+			data: json,
+			traditional: true,
+			dataType: 'html',
+			success: function(result){
+				$.toast('Course added! View added courses on Create Schedule page.', {type: 'success'});
+			}
+		});
 	});
 });
