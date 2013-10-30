6d6607e50ef4b84a8c62e277121d903812ac122b
Jonathan Burnett <jburnett96@gmail.com>
2013-10-30 14:53:37 -0400

    Jump to updated course/models.py

--- course/models.py
+++ course/models.py
@@ -20,19 +20,26 @@ class Course(models.Model):
     dept = models.CharField(max_length=200)
     rmpr = models.CharField(max_length=200)
     
-    def __unicode__(self):  
-        return self.name
+    def __unicode__(self):
+        time = ""
+        if not self.lday == "":
+            times += "{} {}".format(self.lday, self.ltime)
+        if not self.dday == "":
+            times += ", {} {}".format(self.dday, self.dtime)
+        return "{} section {}, {}".format(self.name, self.section, times)
 
 class UserProfile(FacebookModel):
     pastsc = models.CharField(max_length=200 ) #Schedule Model to replace Charfield
     cursc = models.CharField(max_length=200 )
     courses = list()
     user = models.ForeignKey(User, unique=True)
-  
-    
+    #facebook_id = models.BigIntegerField(blank=True, unique=True, null=True)
+    #facebook_email = models.CharField(max_length=200 )
+    #tentative userprofile fields to be changed
+
     def __unicode__(self):
         return self.user.username + "'s User Profile"
-    
+
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
