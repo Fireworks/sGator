7d2d051060fcda55deb96be452ad03a336edd6a1
Jonathan Burnett <jburnett96@gmail.com>
2013-10-29 22:56:31 -0400

    Deleted all the commented out code

--- sgator/algorithm.py
+++ sgator/algorithm.py
@@ -2,9 +2,6 @@ from course.models import Course as DB_Course
 from sgator.models import Schedule
 import itertools
 
-#Courses = [] # This is where we would get the list of Strings for the user's requested courses
-#Results = [] # We will query the database to fill this list with each section for each course in Courses
-
 def get_results(Courses):
     # For the multi-dimension construction of Results to work, 
     # need to initialize a list of empty lists.
@@ -32,14 +29,6 @@ def get_results(Courses):
 def overlaps(class1, class2):
     return any(t==t2 for t in class1[1] for t2 in class2[1])
 
-#for i in range(1,len(Results)):
-#    # add every section from Results that match Courses_[i], if they don't overlap
-#    schedule = Schedule.__init__()
-#    schedule.add(Results(i))
-#    for j in range(1,len(Results)):
-#        schedule.add(Results(j))
-#    Possible_Schedules[i] = schedule
-
 def generate_schedules(Results):
     Possible_Schedules = []
     schedule = Schedule.__init__()
@@ -47,20 +36,3 @@ def generate_schedules(Results):
         if all(not overlaps(s1, s2) for s1, s2 in itertools.combinations(possibility, 2)):
             Possible_Schedules.append(possibility)
     return Possible_Schedules
-
-def generate_schedules_helper(Results,i):
-    possibles = []
-    if i == len(Results): # base case
-        return possibles
-    for j in range(len(Results[i])):
-         
-        possibles.append(schedule)
-    else:
-        # If element i+1 !overlap and is not same course, add it to schedule
-        # If element i+1 !overlap and is same course, but is different dl, add to sched
-        # If element i+1 !overlap and is same course, but is same dl, make new schedule
-
-        if (not overlaps(Results[i],Results[i+1])):
-           if ( 
-                possibles.extend( generate_schedules_helper(
-    return possibles
