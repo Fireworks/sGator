5db90d49f908ba4d9fec6cf9985be5a66da053d2
Jonathan Burnett <jburnett96@gmail.com>
2013-10-30 09:30:09 -0400

    Added get_times method to handle cases like u'9-11'

--- sgator/algorithm.py
+++ sgator/algorithm.py
@@ -15,16 +15,24 @@ def get_results(Courses):
             database_results = DB_Course.objects.filter(name__icontains = course)
         sections = []
         for result in database_results:
-            lecture_days = (c for c in result.lday if c is not " ")
-            lecture_time = result.ltime
-            discussion_days = (c for c in result.dday if c is not " ")
-            discussion_time = result.dtime
+            lecture_days = [str(c) for c in result.lday if c is not " "]
+            lecture_times = get_times(result.ltime)
+            lecture_time = str(result.ltime)
+            discussion_days = (str(c) for c in result.dday if c is not " ")
+            discussion_time = str(result.dtime)
             times = [{'day': day, 'time': time} for day, time in zip(lecture_days, lecture_time)] + [{'day': day, 'time': time} for day, time in zip(discussion_days, discussion_time)]
             sections.append((result, times))
         Results.append(sections)
     # At this point, Results contains all the sections of all the courses the user requested
     # With each course split into a dimension
     return Results
+
+def get_times(ltime):
+    times = []
+    r = [int(str(time)) for time in ltime.split('-')]
+    for i in range(r[len(r)-1] - r[0] + 1):
+        times.append(int(str(ltime[0])) + i)
+    return times
 
 # example use: if( overlaps( Results[x][0], Results[x][1] )
 def overlaps(class1, class2):
