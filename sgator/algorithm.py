1146cfb3e9bd9a68d70655f466e506a1f9f34ce5
Jonathan Burnett <jburnett96@gmail.com>
2013-10-29 22:42:18 -0400

    Fixing stuff, about to make it no longer recursive

--- sgator/algorithm.py
+++ sgator/algorithm.py
@@ -1,5 +1,6 @@
 from course.models import Course as DB_Course
 from sgator.models import Schedule
+import itertools
 
 #Courses = [] # This is where we would get the list of Strings for the user's requested courses
 #Results = [] # We will query the database to fill this list with each section for each course in Courses
@@ -8,43 +9,28 @@ def get_results(Courses):
     # For the multi-dimension construction of Results to work, 
     # need to initialize a list of empty lists.
     Results = []
-    for x in range(len(Courses)):
-        res.append([]) 
-    num_courses = 0
     for course in Courses:
         course.replace(" ","") # Remove any spaces
         if course.isdigit(): # If a course entry has numbers, the user is requesting any section
-            piece = list(DB_Course.objects.filter(id = course))
+            database_results = DB_Course.objects.filter(id = course)
         else: # If a course entry is just numbers, the user is requesting a specific section
-            piece = list(DB_Course.objects.filter(name__icontains = course))
-        for i in range(len(piece)):
-            lecture_days = (c for c in piece[i].lday if c is not " ")
-            lecture_time = piece[i].ltime
-            discussion_days = (c for c in piece[i].dday if c is not " ")
-            discussion_time = piece[i].dtime
+            database_results = DB_Course.objects.filter(name__icontains = course)
+        sections = []
+        for result in database_results:
+            lecture_days = (c for c in result.lday if c is not " ")
+            lecture_time = result.ltime
+            discussion_days = (c for c in result.dday if c is not " ")
+            discussion_time = result.dtime
             times = [{'day': day, 'time': time} for day, time in zip(lecture_days, lecture_time)] + [{'day': day, 'time': time} for day, time in zip(discussion_days, discussion_time)]
-            discussion_flag = 0 # Set a flag equal to 1 if the section is a discussion
-            if (piece[i].lday = '') 
-                discussion_flag = 1
-            piece[i] = (Results[i], times, discussion_flag)
-            Results[num_courses].extend(piece[i])
-            num_courses += 1
+            sections.append((result, times))
+        Results.append(sections)
     # At this point, Results contains all the sections of all the courses the user requested
+    # With each course split into a dimension
     return Results
 
 # example use: if( overlaps( Results[x][0], Results[x][1] )
 def overlaps(class1, class2):
     return any(t==t2 for t in class1[1] for t2 in class2[1])
-
-# example use: if( samecourse( Results[y][0], Results[y][1] )
-# Returns True if both classes are the same course by name
-def samecourse(class1, class2):
-    return class1[0].name == class2[0].name
-
-# example use: if( both_dl( Results[z][0], Results[z][1] )
-# Returns True if both classes are discussions or both are lectures
-def both_dl(class1, class2):
-    return class1[2] == class2[2]
 
 #for i in range(1,len(Results)):
 #    # add every section from Results that match Courses_[i], if they don't overlap
